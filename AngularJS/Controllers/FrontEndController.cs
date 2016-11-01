4b311499e96505b920e1643fdaabb15d13449e9f
wsion <waychanmail@gmail.com>
2016-11-01 10:14:22 +0800

    Initialization

---
+++ AngularJS/Controllers/FrontEndController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularJS.Controllers
+{
+    //[RoutePrefix("AngularJS")]
+    public class FrontEndController : Controller
+    {
+        //[Route("Content/{viewname:string}")]
+        public ActionResult Index(string viewname)
+        {
+            return View(viewname);
+        }
+
+        public ActionResult Partial(string viewname)
+        {
+            return PartialView(viewname);
+        }
+    }
+}
